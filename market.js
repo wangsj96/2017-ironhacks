bbec6bc86bd1d1d55468c1ac1eaf400b2f1fba06
wangsj96 <wangsijin96@gmail.com>
2017-04-17 21:57:35 -0400

    markets markers created

--- market.js
+++ market.js
@@ -7,6 +7,44 @@
 			getWeather(zipcode);
 			getMarkets(zipcode);
 		});
+
+		var geocoder;
+  		var map;
+		function initMap(zip){
+  			geocoder = new google.maps.Geocoder();
+  			var myOptions = {
+  			    zoom: 9,
+  			    center: codeAddress(zip)
+  			}
+  			map = new google.maps.Map(document.getElementById("map"), myOptions);
+ 		};
+ 		function codeAddress(zipCode) {
+ 		    geocoder.geocode( { 'address': zipCode}, function(results, status) {
+ 		      if (status == google.maps.GeocoderStatus.OK) {
+ 		        //Got result, center the map and put it out there
+ 		        map.setCenter(results[0].geometry.location);
+ 			    var marker = new google.maps.Marker({
+ 		            map: map,
+ 		            position: results[0].geometry.location,
+ 		            title: 'Your position'
+ 		        });
+ 		        var infowindow = new google.maps.InfoWindow({
+ 				    content: "<span>Your position</span>",
+ 				    disableAutoPan: true
+ 				});
+ 				infowindow.open(map,marker);
+ 				/*google.maps.event.addListener(marker, 'mouseover', function() {
+ 				  	infowindow.open(map,marker);
+ 				});
+ 				
+ 				google.maps.event.addListener(marker, 'mouseout', function() {
+ 				  	infowindow.close(map,marker);
+ 				});*/
+ 		      } else {
+ 		        alert("Geocode was not successful for the following reason: " + status);
+ 		      }
+ 		    });
+ 		  }
 		
 		function getWeather(zip) {
 			//get today's date
@@ -31,7 +69,7 @@
 				if(result.results[i].datatype == 'TOBS') {
 					//console.log(result.results[i].value);
 					var temp = result.results[i].value.toString().slice(0, -1) + '.' + result.results[i].value.toString().slice(-1);
-					var p = $("<p>Today's temperature is <b>" + temp + "°C</b></p>");
+					var p = $("<p>Today's temperature is about <b>" + temp + "°C</b></p>");
 					break;
 				}
 			}
@@ -40,7 +78,7 @@
 
 		//get local markets
 		var markets;
-		var address = new Array;
+		//var counter = 0;
 
 		function getMarkets(zip) {
 			$.ajax({
@@ -55,16 +93,11 @@
 		//get the market ids from the json response.
 		function getMarketID(result) {
 			//console.log(result.results);
-			markets = new Array;
+			markets = Create2DArray(result.results.length);
 			var counter = 0;
-			for(id in result.results) {
-				markets[counter] = result.results[counter].id;
-				counter ++;
-			}
-			//create table with market information append to div#markets
 			var mytable = $("<table></table>");
 			mytable.append("<tr><th>Market Name</th><th>Distance to you</th></tr>");
-			for(counter = 0; counter < result.results.length; counter ++) {
+			for(id in result.results) {
 
 				var allName = result.results[counter].marketname;
 				var index = allName.indexOf(" ");
@@ -73,66 +106,75 @@
 
 				mytable.append("<tr><td>" + name + "</td><td>"
 								+ dist + " miles </td></tr>");
+				markets[counter].id = result.results[counter].id;
+				markets[counter].distance = dist;
+				markets[counter].name = name;
+				counter ++;
 			}
 
 			$("#markets").append(mytable);
 			//console.log(markets);
 			//get market detail
-			for(var i = 0; i < markets.length; i ++) {
+			for(counter = 0; counter < markets.length; counter ++) {
 				$.ajax({
 			        type: "GET",
 			        contentType: "application/json; charset=utf-8",
 			        // submit a get request to the restful service mktDetail.
-			        url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + markets[i],
+			        url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + markets[counter].id,
 			        dataType: 'jsonp',
-		        	jsonpCallback: 'createMarker'
+		        	success: function(data){
+		        		var start = this.url.indexOf('=') + 1 ;
+		        		var end = this.url.indexOf('&');
+		        		var id = this.url.substring(start, end);
+		        		//console.log(id);
+		        		for (var n = 0; n < markets.length; n ++) {
+		        			if(markets[n].id == id) {
+		        				markets[n].address = data.marketdetails.Address;
+		        				markets[n].link = data.marketdetails.GoogleLink;
+		        				markets[n].time = data.marketdetails.Schedule;
+		        				markets[n].category = data.marketdetails.Products;
+		        				var latStart = markets[n].link.indexOf("=") + 1;
+								var latEnd = markets[n].link.indexOf("%2C");
+								var lngStart = markets[n].link.indexOf("%20") + 3;
+								var lngEnd = markets[n].link.indexOf("(") - 3;
+								var lat = parseFloat(markets[n].link.substring(latStart, latEnd));
+								var lng = parseFloat(markets[n].link.substring(lngStart,lngEnd));
+								//console.log(lat+','+lng);
+								var marker = new google.maps.Marker({
+				 		            map: map,
+				 		            position: {lat: lat, lng: lng}
+				 		    	});
+				 		    	var infowindow = new google.maps.InfoWindow({
+				 				    content: markets[n].name,
+				 				    disableAutoPan: true
+				 				});
+				 				
+				 				google.maps.event.addListener(marker, 'mouseover', function() {
+				 				  	infowindow.open(map,marker);
+				 				});
+				 				//infowindow.open(map,marker);
+				 				google.maps.event.addListener(marker, 'mouseout', function() {
+				 				  	infowindow.close(map,marker);
+				 				});
+		        				break;
+		        			}
+		        		}
+		        	}
 		    	});
-			};
+			}
 		}
-		//iterate through the JSON result object.
-		function createMarker(detailresults) {
-		    var counter = address.length;
-			address[counter] = detailresults.marketdetails.Address;
-			$('#data').append(address +  "<br />");
+		function log() {
+			console.log(markets[1].link);
+		}
+		//initiate array with several related fields
+		function Create2DArray(rows) {
+		  var arr = [];
+
+		  for (var i=0;i<rows;i++) {
+		     arr[i] = {id:"",name:"",address:"",link:"", time:"", category:"", distance:""};
+		  }
+
+		  return arr;
 		}
 
-
-		var geocoder;
-  		var map;
-		function initMap(zip){
-  			geocoder = new google.maps.Geocoder();
-  			var myOptions = {
-  			    zoom: 12,
-  			    center: codeAddress(zip)
-  			}
-  			map = new google.maps.Map(document.getElementById("map"), myOptions);
-  			var marker = new google.maps.Marker({ //Line 1
- 				position: {lat: 40.4237, lng: -86.9212}, //Line2: Location to be highlighted
- 				map: map,//Line 3: Reference to map object
- 				title: 'Purdue University' //Line 4: Title to be given
- 			})
- 		};
- 		function codeAddress(zipCode) {
- 		    geocoder.geocode( { 'address': zipCode}, function(results, status) {
- 		      if (status == google.maps.GeocoderStatus.OK) {
- 		        //Got result, center the map and put it out there
- 		        map.setCenter(results[0].geometry.location);
- 			    var marker = new google.maps.Marker({
- 		            map: map,
- 		            position: results[0].geometry.location,
- 		            title: 'Your position'
- 		        });
- 		        var infowindow = new google.maps.InfoWindow({
- 				    content: "<span>Your position</span>"
- 				});
- 				google.maps.event.addListener(marker, 'mouseover', function() {
- 				  	infowindow.open(map,marker);
- 				});
- 				google.maps.event.addListener(marker, 'mouseout', function() {
- 				  	infowindow.close(map,marker);
- 				});
- 		      } else {
- 		        alert("Geocode was not successful for the following reason: " + status);
- 		      }
- 		    });
- 		  }
+		
